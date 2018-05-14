0292ec8a9966c5f3ce325cebd0dbb2c6c1dcf15f
earl_b <earlmark.barangot@g.msuiit.edu.ph>
2018-05-14 02:34:10 -0700

    some

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
 from app import app
+import os
 
 if __name__=='__main__':
-	app.run(threaded=True)
+	#app.run(threaded=True)
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
